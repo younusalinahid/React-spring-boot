df0e8ad8345dd55db4362164735748b2ff6516d9
nahid <eng.yanahid@gmail.com>
2024-04-27 14:15:51 +0600

    Create a React.js project and write some component

---
+++ src/components/learning-examples/SecondComponent.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+
+//class Component
+export  default class FirstComponent extends Component {
+    render() {
+        return (
+            <div className="FirstComponent">
+                First Component
+            </div>
+        );
+    }
+}
