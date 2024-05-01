8df5072c65918cad600289a4e3f746889e32f293
nahid <eng.yanahid@gmail.com>
2024-05-01 21:59:58 +0600

    Understanding React Component Props-Default Value and Type Constrain

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -1,7 +1,7 @@
 import React, {Component} from 'react'
+import PropTypes from 'prop-types'
 import './Counter.css'
 class Counter extends Component {
-
     //Define the initial state in constructor;
     //state => counter 0;
     constructor() {
@@ -15,7 +15,7 @@ class Counter extends Component {
     render() {
         return (
             <div className="counter">
-                <button onClick={this.increment} >+1</button>
+                <button onClick={this.increment} >+{this.props.by}</button>
                 <span className="count" > {this.state.counter}</span>
             </div>
         )
@@ -24,9 +24,17 @@ class Counter extends Component {
     //Update statement counter++;
     increment() {
         this.setState({
-            counter: this.state.counter + 1
+            counter: this.state.counter + this.props.by
         });
     }
+}
+
+Counter.defaultProps = {
+    by : 1
+}
+
+Counter.propTypes = {
+    by : PropTypes.number
 }
 
 export default Counter
