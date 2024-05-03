d7f23126f5f7f2dd0378386736d67b968e13eeb4
nahid <eng.yanahid@gmail.com>
2024-05-03 17:51:17 +0600

    Todo Management App implemente login component and welcome component

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ import ThirdComponent from './components/learning-examples/ThirdComponent'
 import CounterButton from "./components/counter/Counter";
 import './App.css'
 import Counter from "./components/counter/Counter";
+import TodoApp from "./components/todo/TodoApp";
 
 
 class App extends Component {
     render() {
         return (
             <div className="App">
-                <Counter/>
+                {/*<Counter/>*/}
+                <TodoApp/>
             </div>
         );
     }
