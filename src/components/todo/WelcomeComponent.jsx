8db46aa6a84bdd4f17fe1728283317b7f02d2542
nahid <eng.yanahid@gmail.com>
2024-05-04 19:48:46 +0600

    Connecting React with Restful Api and calling all Api from React Frontend

--- src/components/todo/WelcomeComponent.jsx
+++ src/components/todo/WelcomeComponent.jsx
@@ -1,17 +1,58 @@
-import {Link, useParams} from "react-router-dom";
-import React from "react";
+import React, {Component} from "react";
+import {Link} from "react-router-dom";
+import HelloWorldService from "../../api/todo/HelloWorldService";
 
-function WelcomeComponent() {
-    const {name} = useParams();
-    return (
-        <>
-            <h1>Welcome!</h1>
-            <div className="container">
-                Welcome {name}. You can manage your todos <Link to="/todos">here</Link>
-            </div>
-        </>
+class WelcomeComponent extends Component{
 
-    );
+    constructor(props) {
+        super(props);
+        this.retrieveWelcomeMessage = this.retrieveWelcomeMessage.bind(this)
+        this.state = {
+            welcomeMessage: ''
+        };
+        this.handleSuccessfulResponse = this.handleSuccessfulResponse.bind(this)
+        // this.handleError = this.handleError.bind(this)
+    }
+
+    render() {
+        const { name } = this.props; // Destructure name from props
+
+        return (
+            <>
+                <h1>Welcome!</h1>
+                <div className="container">
+                    {/* eslint-disable-next-line no-restricted-globals */}
+                    Welcome {name}.
+                    You can manage your todos <Link to="/todos">here</Link>
+                </div>
+                <div className="container">
+                    Click here to get a customized welcome message.
+                    <button onClick={this.retrieveWelcomeMessage}
+                            className="btn btn-success">Get Welcome message</button>
+                </div>
+                <div className="container">
+                    {this.state.welcomeMessage}
+                </div>
+            </>
+        );
+    }
+
+    retrieveWelcomeMessage() {
+        // HelloWorldService.executeHelloWorldService()
+        // .then( response => this.handleSuccessfulResponse(response) )
+
+        // HelloWorldService.executeHelloWorldBeanService()
+        // .then( response => this.handleSuccessfulResponse(response) )
+
+        HelloWorldService.executeHelloWorldPathVariableService(this.props.params.name)
+            .then(response => this.handleSuccessfulResponse(response))
+            //.catch(error => this.handleError(error))
+    }
+
+    handleSuccessfulResponse(response) {
+        console.log(response)
+        this.setState({welcomeMessage: response.data.message})
+    }
 }
 
 export default WelcomeComponent;
