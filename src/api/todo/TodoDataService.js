ecedf24b673e2f39c3d4146ca417bcf05de16921
nahid <eng.yanahid@gmail.com>
2024-05-25 20:16:57 +0600

    change TodoDataService Api endpoint

--- src/api/todo/TodoDataService.js
+++ src/api/todo/TodoDataService.js
@@ -5,23 +5,23 @@ class TodoDataService {
 
     retrieveAllTodos(name) {
         console.log('executed service')
-        return axios.get(`${API_URL}/users/${name}/todos`);
+        return axios.get(`${JPA_API_URL}/users/${name}/todos`);
     }
 
     retrieveTodo(name, id) {
-        return axios.get(`${API_URL}/users/${name}/todos/${id}`);
+        return axios.get(`${JPA_API_URL}/users/${name}/todos/${id}`);
     }
 
     deleteTodo(name, id) {
-        return axios.delete(`${API_URL}/users/${name}/todos/${id}`);
+        return axios.delete(`${JPA_API_URL}/users/${name}/todos/${id}`);
     }
 
     updateTodo(name, id, todo) {
-        return axios.put(`${API_URL}/users/${name}/todos/${id}`, todo);
+        return axios.put(`${JPA_API_URL}/users/${name}/todos/${id}`, todo);
     }
 
     createTodo(name, todo) {
-        return axios.post(`${API_URL}/users/${name}/todos`, todo);
+        return axios.post(`${JPA_API_URL}/users/${name}/todos`, todo);
     }
 }
 
